54f08d8ab484cbf69dbd0f1fbe69d08b5084bd08
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-12-07 07:27:27 -0800

    Update answer key for JS foundations

--- implementation.js
+++ implementation.js
@@ -4,12 +4,65 @@ function square(n) {
   return n * n;
 }
 
+function addTwo(n1, n2) {
+  return n1 + n2;
+}
 
-// Here is an example to run tests with the command `npm test` IN YOUR TERMINAL
-// module.exports = {
-//   square: function(n) {
-//     return n * n;
-//   }
-//
-//
-// }
+function multiply(n1, n2) {
+	return n1 * n2;
+}
+
+function divide(n1, n2) {
+	return n1 / n2;
+}
+
+function myReverseFunction(string){
+  var newString = '';
+  
+  for (var i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+  return newString;
+}
+
+function findLongestWord(str) {
+  var strSplit = str.split(' ');
+  console.log(strSplit)
+  var longestWordLength = 0;
+  var longestWord = "";
+
+  for(var i = 0; i < strSplit.length; i++) {
+    if(strSplit[i].length > longestWordLength){
+      longestWordLength = strSplit[i].length;
+      longestWord = strSplit[i]
+     }
+  }
+  return longestWord;
+}
+
+function convertTime(minutes) {
+   var hours = Math.floor(minutes / 60)
+   var convertedMinutes = minutes - (hours * 60)
+  
+  if(minutes < 10){
+    return '0:0' + minutes;
+  } else if (minutes >= 10 && minutes < 60){
+   return '0:' + minutes;
+  } else if (minutes > 60 && convertedMinutes < 10) {
+   return hours + ":0" + convertedMinutes
+  } else {
+    return hours + ":" + convertedMinutes
+  }
+}
+
+function fizzBuzz(n) {
+  if(n % 5 === 0 && n % 3 === 0) {
+      return 'FizzBuzz';
+    } else if ( n % 3 === 0) {
+      return 'Fizz';
+    } else if (n % 5 === 0) {
+      return 'Buzz';
+    } else {
+      return n;
+    }
+}
--- test.js
+++ test.js
@@ -7,3 +7,53 @@ describe('square', function () {
     assert.equal(square(2), 4);
   });
 });
+
+describe('addTwo', function () {
+	it('return the sum of two numbers', function () {
+		
+		var expected = 6;
+		var actual = addTwo(4, 2);
+
+		assert.equal(actual, expected);
+	});
+});
+
+describe('multiply', function() {
+	it('return the product of two numbers', function () {
+		assert.equal(multiply(3, 3), 9);
+	});
+});
+
+describe('divide', function () {
+	it('return the quotient of two numbers', function () {
+		assert.equal(divide(10, 5), 2)
+	});
+});
+
+describe('myReverseFunction', function() {
+	it('should reverse a string', function () {
+		assert.equal(myReverseFunction('reverse'), 'esrever');
+	})
+})
+
+describe('findLongestWord', function() {
+	it('should find the longest word in a string', function () {
+		assert.equal(findLongestWord('We had a great time!'), 'great');
+	})
+})
+
+describe('convertTime', function() {
+	implementation('should convert minutes to hours and minutes', function () {
+		assert.equal(convertTime(126),'2:06');
+		assert.equal(convertTime(45), '0:45');
+	})
+})
+
+describe('fizzBuzz', function() {
+	it('should return a number when passed a number, multiples of 3 should return Fizz, multiples of 4 should return Buzz, multiples of 3 and 5 should return FizzBuzz', function () {
+		assert.equal(fizzBuzz(1), 1);
+		assert.equal(fizzBuzz(9), 'Fizz');
+		assert.equal(fizzBuzz(20), 'Buzz');
+		assert.equal(fizzBuzz(15), 'FizzBuzz');
+	})
+})
